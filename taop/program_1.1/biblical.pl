a811f7b12f715088b0d69da664c106816251d567
apaz <apaz@atech.com.br>
2018-03-12 11:56:41 -0300

    Program of section 1.1

---
+++ taop/program_1.1/biblical.pl
@@ -0,0 +1,23 @@
+father(terach, abraham).
+father(terach, nachor).
+father(terach, haran).
+
+father(abraham, isaac).
+
+father(haram, lot).
+father(haram, milcah).
+father(haram, yiscah).
+
+mother(sarah, isaac).
+
+male(terach).
+male(abraham).
+male(nachor).
+male(haran).
+male(isaac).
+male(lot).
+
+female(sarah).
+female(milcah).
+female(yiscah).
+
